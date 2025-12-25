96641bdecea0fb83b7b2d512ea264a1aa5c1934c
busra <37024872+bsrayapici@users.noreply.github.com>
2025-12-25 18:37:29 +0300

    jpa auditing config is added

---
+++ src/main/java/com/workintech/s18d4/config/JpaAuditConfig.java
@@ -0,0 +1,12 @@
+package com.workintech.s18d4.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+@Profile("!test")
+public class JpaAuditConfig {
+}
